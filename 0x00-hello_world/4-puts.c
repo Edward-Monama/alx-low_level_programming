e690f413cfd510c3bc347e8df3e95360530761bf
edwardmonama <monamemonama@gmail.com>
2023-02-16 12:43:49 -0800

    hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * A C program that prints "programming is like building a multilihgual puzzle
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
