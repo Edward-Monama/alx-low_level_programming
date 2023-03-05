b5c00b62fff7d7e42a5339a7e45f8a42d4fb634b
edwardmonama <monamemonama@gmail.com>
2023-03-05 12:03:02 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ * return - always (0)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstvwxyz";
+	int i;
+	for (i = 0, i < 26, i++);
+	{
+		putchar (alp[i], "\n");
+	}
+	return(0);
+}
